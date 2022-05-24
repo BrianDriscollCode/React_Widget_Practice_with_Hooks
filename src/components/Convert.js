c421175fb29cd1b49489e52badd6ca5fbd1a7eab
briandriscollcode <bdriscoll407@gmail.com>
2022-05-23 22:18:21 -0700

    finished last component, now adding navigation

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -3,24 +3,59 @@ import axios from 'axios'
 
 const Convert = ( {language, text} ) => {
 
+    const [translated, setTranslated] = useState('');
+    const [debouncedText, setDebouncedText] = useState('text');
+
+
     useEffect(() => {
+
+        const timerId = setTimeout(() => {
+
+            setDebouncedText(text);
+
+
+        }, 500); 
+
+        return () => {
+
+            clearTimeout(timerId);
+
+        }
+
+    }, [text]) 
+
+    useEffect(() => {
+
+        const doTranslation = async () => {
+
+            const {data} = await axios.post('https://translation.googleapis.com/language/translate/v2', 
+                {}, 
+                {
+                    params: {
+                        q: debouncedText,
+                        target: language.value,
+                        key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+                    }
+                }
+            );
+
+            setTranslated(data.data.translations[0].translatedText)
+        }
+
+        
 
         //this is a post request. 
         //hence the 2nd empty object for the "body"
         //but we still use the 3rd object for parameters
-        axios.post('https://translation.googleapis.com/language/translate/v2', 
-        {}, 
-        {
-            params: {
-                q: text,
-                target: language.value,
-                key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-            }
-        })
+        doTranslation();
 
-    }, [language, text])
+    }, [language, debouncedText])
 
-    return <div> </div>
+    return (
+        <div>
+            <h1 className='ui header'>{translated}</h1>
+        </div>    
+    )
 
 }
 
